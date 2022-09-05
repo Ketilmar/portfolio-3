9d7539855d39b6d6b90091a87ece0c9bdddb8b72
Ketil Kodehode <ketilmar.kodehode@gmail.com>
2022-09-05 14:43:38 +0200

    fixed some. Added top-menu

---
+++ src/components/top-menu.jsx
@@ -0,0 +1,9 @@
+const TopMenu = () => {
+  return (
+    <div className="top-menu">
+      <div> dette er top menyen</div>
+    </div>
+  );
+};
+
+export { TopMenu };
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import { Projectlist } from "./projects/projectList";
+import { TopMenu } from "../components/top-menu";
 
 const MainPage = () => {
   return (
     <div className="main-page">
+      <TopMenu />
       <Routes>
         <Route path="/projectlist" element={<Projectlist />} />
       </Routes>
